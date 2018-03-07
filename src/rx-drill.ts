a31f3ad9043f87b5499816c24d7c51b4972270e3
Nadav Sinai <nadav@algotec.co.il>
2018-03-07 10:29:35 +0200

    simple filtering

--- src/rx-drill.ts
+++ src/rx-drill.ts
@@ -1,12 +1,14 @@
 import {Observable} from 'rxjs/observable';
 import {fromEvent} from 'rxjs/observable/fromEvent';
-
+import 'rxjs/add/operator/filter';
 
 const keyDown$: Observable<KeyboardEvent> = fromEvent(document, 'keydown');
 
-keyDown$.subscribe((keyEvent) => {
-  console.log(keyEvent);
-});
+keyDown$
+  .filter(key => key.code.includes('Arrow'))
+  .subscribe((keyEvent) => {
+    console.log(keyEvent);
+  });
 
 
 
