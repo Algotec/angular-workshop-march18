efcb3b6c1c21c3bc9957f2a86dcfed0bac261852
Nadav Sinai <nadav@algotec.co.il>
2018-03-07 10:32:44 +0200

    refactor for pipeable operators

--- src/rx-drill.ts
+++ src/rx-drill.ts
@@ -1,11 +1,12 @@
 import {Observable} from 'rxjs/observable';
 import {fromEvent} from 'rxjs/observable/fromEvent';
-import 'rxjs/add/operator/filter';
+import {filter} from 'rxjs/operators/filter';
 
 const keyDown$: Observable<KeyboardEvent> = fromEvent(document, 'keydown');
 
 keyDown$
-  .filter(key => key.code.includes('Arrow'))
+  .pipe(
+    filter(key => key.code.includes('Arrow')))
   .subscribe((keyEvent) => {
     console.log(keyEvent);
   });
