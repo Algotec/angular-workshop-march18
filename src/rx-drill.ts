624bf797f0d91dac2596c26246122d661775ca30
Nadav Sinai <nadav@algotec.co.il>
2018-03-07 10:33:04 +0200

    expain how pipe works

--- src/rx-drill.ts
+++ src/rx-drill.ts
@@ -2,11 +2,10 @@ import {Observable} from 'rxjs/observable';
 import {fromEvent} from 'rxjs/observable/fromEvent';
 import {filter} from 'rxjs/operators/filter';
 
+
 const keyDown$: Observable<KeyboardEvent> = fromEvent(document, 'keydown');
 
-keyDown$
-  .pipe(
-    filter(key => key.code.includes('Arrow')))
+filter((key: KeyboardEvent) => key.code.includes('Arrow'))(keyDown$)
   .subscribe((keyEvent) => {
     console.log(keyEvent);
   });
