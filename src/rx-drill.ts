80ea6463c59f95c0471aee64a27c96457824d5f8
Nadav Sinai <nadav@algotec.co.il>
2018-03-07 10:44:56 +0200

    side effects using subscribe

--- src/rx-drill.ts
+++ src/rx-drill.ts
@@ -1,13 +1,30 @@
 import {Observable} from 'rxjs/observable';
 import {fromEvent} from 'rxjs/observable/fromEvent';
 import {filter} from 'rxjs/operators/filter';
-
+import {tap} from 'rxjs/operators/tap';
 
 const keyDown$: Observable<KeyboardEvent> = fromEvent(document, 'keydown');
-
-filter((key: KeyboardEvent) => key.code.includes('Arrow'))(keyDown$)
+const img: HTMLImageElement = document.querySelector('#logo') as HTMLImageElement;
+keyDown$
+  .pipe(
+    filter(key => key.code.includes('Arrow')),
+    tap((keyEvent) => console.log(keyEvent)))
   .subscribe((keyEvent) => {
-    console.log(keyEvent);
+    let val = 0;
+    switch (keyEvent.code) {
+      case 'ArrowUp':
+        val = -10;
+        break;
+      case 'ArrowDown':
+        val = +10;
+        break;
+    }
+    let top = parseInt(img.style.top.replace('px', ''), 10);
+    if (isNaN(top)) {
+      top = 0;
+    }
+    img.style.top = top + val + 'px';
+
   });
 
 
