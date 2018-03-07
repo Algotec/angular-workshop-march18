8a17b9f3cc06cffd2b53cb5a74c432ee21cedcb9
Nadav Sinai <nadav@algotec.co.il>
2018-03-07 10:27:36 +0200

    convert to fromEvent

--- src/rx-drill.ts
+++ src/rx-drill.ts
@@ -1,19 +1,12 @@
 import {Observable} from 'rxjs/observable';
+import {fromEvent} from 'rxjs/observable/fromEvent';
 
-const obs1 = Observable.create(function (observer) {
-  // can setup the data source here
-  // then can call next/error/completed on the observer here
-  console.log('creation');
-  const listener = function (event) {
-    observer.next(event);
-  };
-  document.addEventListener('keydown', listener);
-  return function () {
-// can do any teardown logic here
-    document.removeEventListener('keydown', listener);
-  };
+
+const keyDown$: Observable<KeyboardEvent> = fromEvent(document, 'keydown');
+
+keyDown$.subscribe((keyEvent) => {
+  console.log(keyEvent);
 });
 
-obs1.subscribe(console.log);
 
 
