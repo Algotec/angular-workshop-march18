a390eb6d6feeeace40b2427ad99cd5f6e05ddd6b
Nadav Sinai <nadav@algotec.co.il>
2018-03-14 16:50:05 +0200

    ----exercise 1 solution --- clock component in own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {PetsModule} from './pets/pets.module';
 import {SharedModule} from './shared/shared.module';
+import {TimeModule} from './time/time.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {SharedModule} from './shared/shared.module';
     BrowserModule,
     SharedModule,
     PetsModule,
+    TimeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
