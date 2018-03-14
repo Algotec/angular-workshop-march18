877094a64eabfff5d51cf4e48f1324f9514a26f9
Nadav Sinai <nadav@algotec.co.il>
2018-03-14 16:05:28 +0200

    connect pet-list

--- src/app/pets/pet-list.component.ts
+++ src/app/pets/pet-list.component.ts
@@ -16,14 +16,15 @@ import {PetService} from './pet.service';
     <section>
       <h2>Pet List</h2>
       <ul>
-        <!-- Pets would be iterated here using LI tag for each one -->
+        <li *ngFor="let petName of petService.pets">
+          <span class="petName">{{petName}}</span>
+        </li>
       </ul>
     </section>
   `
 })
 export class PetListComponent {
   constructor(public petService: PetService) {
-
   }
 
 
