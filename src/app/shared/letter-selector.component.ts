b2a5b861bf6f51a487521ba730aded37a0b64dbb
Nadav Sinai <nadav@algotec.co.il>
2018-03-14 16:36:19 +0200

    add letter selector component not hooked

---
+++ src/app/shared/letter-selector.component.ts
@@ -0,0 +1,33 @@
+import {Component, Output, EventEmitter, Input} from '@angular/core';
+
+@Component({
+  selector: 'letter-selector',
+  styles: [`ul {
+    list-style: none
+  }`, `li {
+    display: inline-block;
+    padding: 0 5px;
+  }`, `.selected {
+    background-color: lightblue
+  }`],
+  template: `
+    <ul>
+      <li>
+        <button [class.selected]="letterSelected === ''" (click)="selectLetter('')">Show All</button>
+      </li>
+      <li *ngFor="let letter of letters">
+        <button [class.selected]="letter === letterSelected" (click)="selectLetter(letter)">{{letter}}</button>
+      </li>
+    </ul>
+  `
+})
+export class LetterSelectorComponent {
+  @Output('select') selectEmitter = new EventEmitter(); // inputs/outputs can be renamed
+  letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  @Input('letter') letterSelected = '';
+
+  selectLetter(letter: string) {
+    this.letterSelected = letter;
+    this.selectEmitter.emit(this.letterSelected);
+  }
+}
