dd89e8c587403ad0593e75763db6ac987594131e
Nadav Sinai <nadav@algotec.co.il>
2018-03-14 16:32:19 +0200

    pet search static letter

---
+++ src/app/pets/pet-search.pipe.ts
@@ -0,0 +1,10 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {PetModel} from './pet.model';
+
+@Pipe({name: 'petSearch'})
+export class PetSearchPipe implements PipeTransform {
+  transform(pets: PetModel[], ...args: any[]): any {
+    return pets.filter((p) => p.name.toLowerCase().startsWith('c'));
+  }
+
+}
