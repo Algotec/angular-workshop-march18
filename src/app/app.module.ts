1f3517278323724c8b52f128002133b45fc0ea95
Nadav Sinai <nadav@algotec.co.il>
2018-03-14 16:04:31 +0200

    pet-list initial
    pet-list initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from '@angular/core';
 import {PetService} from './pets/pet.service';
 import {AppComponent} from './app.component';
 import {PetInputComponent} from './pets/pet-input.component';
+import {PetListComponent} from './pets/pet-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PetInputComponent
+    PetInputComponent,
+    PetListComponent
   ],
   imports: [
     BrowserModule
