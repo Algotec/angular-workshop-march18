0eefa4dfe1b3977f39ca8900cedb7dc04809798f
Nadav Sinai <nadav@algotec.co.il>
2018-03-25 16:37:25 +0300

    show the case for observable params - re use the same component only changing route accepted params also speak about keeping subscriptions

--- src/app/shop/shop-detail.component.ts
+++ src/app/shop/shop-detail.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ShopService} from './shop.service';
 import {IShopItem} from './shop.types';
+import {finalize, map} from 'rxjs/operators';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'shop-detail',
@@ -13,23 +15,37 @@ import {IShopItem} from './shop.types';
       </mat-card-header>
       <mat-card-content>
         <img mat-card-lg-image [src]="getRandomImage()">
-        <h3>{{item?.price | currency:'USD':true}}</h3>
+        <h3>{{item?.price | currency:'USD':'symbol'}}</h3>
       </mat-card-content>
       <mat-card-actions>
         <button mat-raised-button (click)="buyItem()">Buy</button>
+        <button mat-raised-button (click)="nextItem()">Next Item</button>
       </mat-card-actions>
     </mat-card>`
 })
-export class ShopItemDetailsComponent implements OnInit {
+export class ShopItemDetailsComponent implements OnInit, OnDestroy {
   item: IShopItem;
+  private paramsSubscriber: Subscription;
 
   constructor(private activeRoute: ActivatedRoute, private shopService: ShopService, private router: Router) {
   }
 
   ngOnInit() {
-    this.shopService.get(this.activeRoute.snapshot.params['id']).then(item => {
-      this.item = item;
+    this.paramsSubscriber = this.activeRoute.params.pipe(
+      map((params) => params['id']),
+      finalize(() => console.log('finished route sub'))
+    ).subscribe((id) => {
+      this.shopService.get(id).then(item => {
+        this.item = item;
+      });
     });
+  }
+
+  ngOnDestroy() {
+    // if (this.paramsSubscriber) {
+    //   console.log('finished via ngOnDestroy');
+    //   this.paramsSubscriber.unsubscribe();
+    // }
   }
 
   getRandomImage() {
@@ -43,6 +59,10 @@ export class ShopItemDetailsComponent implements OnInit {
   buyItem() {
     this.shopService.addTOCart(this.item);
     this.router.navigate(['/shop']);
+  }
+
+  nextItem() {
+    this.router.navigate(['/shop', 'detail', this.shopService.getNextId(this.item._id)]);
   }
 
 }
--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,18 +1,24 @@
 import {IShopItem} from './shop.types';
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {tap} from 'rxjs/operators';
 
 
 @Injectable()
 export class ShopService {
   private static readonly baseUrl = 'http://localhost:3003/data/petShop';
   private shoppingCart: IShopItem[] = [];
+  private items: IShopItem[] = [];
 
   constructor(private http: HttpClient) {
+    this.getItems();
   }
 
   getItems(): Promise<IShopItem[]> {
     return this.http.get<IShopItem[]>(ShopService.baseUrl)
+      .pipe(tap((items) => {
+        this.items = items;
+      }))
       .toPromise();
   }
 
@@ -27,6 +33,11 @@ export class ShopService {
 
   removeFromCart(index: number) {
     this.shoppingCart = [...this.shoppingCart.slice(0, index), ...this.shoppingCart.slice(index + 1)];
+  }
+
+  getNextId(_id) {
+    const index = this.items.findIndex((i) => _id === i._id);
+    return (index === this.items.length - 1) ? this.items[0]._id : this.items[index + 1]._id;
   }
 
   getCart() {
