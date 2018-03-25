3ab0b1105dcb492a3962ace7e44964c879e4fa55
Nadav Sinai <nadav@algotec.co.il>
2018-03-25 17:20:24 +0300

    add admin routes guard

--- src/app/shop/shop.routes.ts
+++ src/app/shop/shop.routes.ts
@@ -2,6 +2,7 @@ import {Route} from '@angular/router';
 import {ShopFrontComponent} from './shop-front.component';
 import {ShopItemDetailsComponent} from './shop-detail.component';
 import {ShopAdminItemEditComponent} from '../shopAdmin/shop-admin-item-edit.component';
+import {AuthGuard} from '../userAuth/auth.guard';
 
 export const shopRoutes: Route[] = [
   {
@@ -9,7 +10,7 @@ export const shopRoutes: Route[] = [
       {path: '', component: ShopFrontComponent},
       {path: 'detail/:id', component: ShopItemDetailsComponent},
       {
-        path: 'admin', children: [
+        path: 'admin', canActivate: [AuthGuard], children: [
           {path: 'edit/:id', component: ShopAdminItemEditComponent},
           {path: 'add', component: ShopAdminItemEditComponent}
         ]
