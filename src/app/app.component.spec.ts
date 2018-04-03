48fafb93a31d8560d4cbb535a08440c20ccd393d
Nadav Sinai <nadav@algotec.co.il>
2018-04-03 19:05:10 +0300

    fix initial testing setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,19 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {AuthService} from './userAuth/auth.service';
+
 describe('AppComponent', () => {
+  const authMock = {isLoggedIn: false};
+  const title = `Pets 'R' us`;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      providers: [{provide: AuthService, useValue: authMock}],
+      schemas: [NO_ERRORS_SCHEMA], // allows for shallow rendering
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -16,12 +24,12 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual(title);
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain(title);
   }));
 });
