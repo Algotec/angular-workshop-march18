f6bfd459c964a7b639b1598fa7d79ab85710394c
Nadav Sinai <nadav@algotec.co.il>
2018-04-04 15:55:39 +0300

    minimal work to get value to change in UIElementModels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, DoCheck} from '@angular/core';
 import {AuthService} from './userAuth/auth.service';
 import {ButtonModel, ButtonType, UIButtonOptionsModel, UIElementModel} from '@algotec/wmdl-webkit';
 
@@ -7,7 +7,7 @@ import {ButtonModel, ButtonType, UIButtonOptionsModel, UIElementModel} from '@al
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
   title = `Pets 'R' us`;
   // add a general theme via the html-element-settings-directive
   // we also added general styles in styles.css
@@ -15,13 +15,30 @@ export class AppComponent {
 
   linkBtn: UIElementModel<ButtonModel, UIButtonOptionsModel> = new UIElementModel({
     toolID: 'linkBtn',
-    elementModel: new ButtonModel('linkBtn', {text: this.authService.isLoggedIn ? 'PROFILE' : 'LOGIN'}),
+    elementModel: new ButtonModel('linkBtn', {text: this.getLoginText(this.authService.isLoggedIn)}),
     options: new UIButtonOptionsModel({
       showIcon: false,
       showText: true,
       buttonType: ButtonType.RegularButton
     })
   });
+
+  private getLoginText(isLoggedIn: boolean) {
+    return (isLoggedIn) ? 'PROFILE' : 'LOGIN';
+  }
+
+  ngDoCheck() {
+    const oldValue = this.linkBtn.elementModel.text;
+    const newValue = this.getLoginText(this.authService.isLoggedIn);
+    if (oldValue !== newValue) {
+      // this.linkBtn = this.linkBtn.setIn(['elementModel', 'text'], newValue);
+      // would be nice - but won't work - ElementModel is no immutable
+      this.linkBtn = this.linkBtn.set('elementModel',
+        new ButtonModel(this.linkBtn.toolID,
+          {...this.linkBtn.elementModel, ...{text: newValue}})
+      );
+    }
+  }
 
   constructor(public authService: AuthService) {
 
